1e854939add2a1e9bd2cf5d547fe72e0d715f58b
Shalauddin Ahamad Shuza <shuza.sa@gmail.com>
2019-01-30 00:22:12 +0600

    Initial commit

---
+++ api/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"log"
+	"kubernates-go-grpc/pd"
+	"github.com/gorilla/mux"
+	"net/http"
+	"encoding/json"
+	"strconv"
+	"fmt"
+	"time"
+	"golang.org/x/net/context"
+)
+
+/**
+ * :=  created by:  Shuza
+ * :=  create date:  29-Jan-2019
+ * :=  (C) CopyRight Shuza
+ * :=  www.shuza.ninja
+ * :=  shuza.sa@gmail.com
+ * :=  Fun  :  Coffee  :  Code
+ **/
+
+func main() {
+	//	Connect to GCD service
+	conn, err := grpc.Dial("localhost:3000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Dial Failed: %v", err)
+	}
+	gcdClient := pb.NewGCDServiceClient(conn)
+
+	routes := mux.NewRouter()
+	routes.HandleFunc("/", indexHandler).Methods("GET")
+	routes.HandleFunc("/gcd/{a}/{b}", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UFT-8")
+
+		vars := mux.Vars(r)
+		a, err := strconv.ParseUint(vars["a"], 10, 64)
+		if err != nil {
+			json.NewEncoder(w).Encode("Invalid parameter A")
+		}
+		b, err := strconv.ParseUint(vars["b"], 10, 64)
+		if err != nil {
+			json.NewEncoder(w).Encode("Invalid parameter B")
+		}
+
+		ctx, cancel := context.WithTimeout(context.TODO(), time.Minute)
+		defer cancel()
+
+		req := &pb.GCDRequest{A: a, B: b}
+		if resp, err := gcdClient.Compute(ctx, req); err == nil {
+			msg := fmt.Sprintf("GCD Result is %d", resp.Result)
+			json.NewEncoder(w).Encode(msg)
+		} else {
+			msg := fmt.Sprintf("Internal server error: %s", err.Error())
+			json.NewEncoder(w).Encode(msg)
+		}
+	}).Methods("GET")
+
+	fmt.Println("Application is running on : 8080 .....")
+	http.ListenAndServe(":8080", routes)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UFT-8")
+	json.NewEncoder(w).Encode("Server is running")
+}
---
+++ gcd/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net"
+	"log"
+	"kubernates-go-grpc/pd"
+	context "golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+/**
+ * :=  created by:  Shuza
+ * :=  create date:  29-Jan-2019
+ * :=  (C) CopyRight Shuza
+ * :=  www.shuza.ninja
+ * :=  shuza.sa@gmail.com
+ * :=  Fun  :  Coffee  :  Code
+ **/
+
+type server struct{}
+
+func main() {
+	lis, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatalf("Failed to listen:  %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGCDServiceServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+
+}
+
+func (s *server) Compute(cxt context.Context, r *pb.GCDRequest) (*pb.GCDResponse, error) {
+	a, b := r.A, r.B
+	for b != 0 {
+		a, b = b, a%b
+	}
+
+	return &pb.GCDResponse{Result: a}, nil
+}
